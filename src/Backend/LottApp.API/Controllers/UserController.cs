9938ab28a7f1368405a123803328a074cdc2aca5
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2025-06-14 15:42:15 -0400

    Initial commit

---
+++ src/Backend/LottApp.API/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LottApp.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(string),StatusCodes.Status201Created)]
+    public IActionResult Register()
+    {
+        return Created(String.Empty, "Register");
+    }
+}
